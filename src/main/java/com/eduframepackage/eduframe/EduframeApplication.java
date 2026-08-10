39e6373fd47e184183e0e81609640e2e594dec2c
ashendrade <unknowncodebyashendra@gmail.com>
2026-08-10 21:02:58 +0530

    First commit

---
+++ src/main/java/com/eduframepackage/eduframe/EduframeApplication.java
@@ -0,0 +1,13 @@
+package com.eduframepackage.eduframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduframeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduframeApplication.class, args);
+    }
+
+}
